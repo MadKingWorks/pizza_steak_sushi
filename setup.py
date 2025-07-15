8e4e8cf5ad413bdec9c5082280c1ba9fc09d75b6
Rahul Sharma <sharma.uce@gmail.com>
2025-07-15 23:11:53 +0200

    created a start.bat file

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import  os
-#create the structure of the project
-#setup the data folder
+import argparse
 from pathlib import Path
 
-path  = os.getcwd()
+args = argparse.ArgumentParser()
+
+cwd = args.add_argument()
+#create the structure of the project
+#setup the data folder
+
+
+path  = Path()
 data_path = path / Path("/Data")
 if os.path.isdir(data_path) :
     print(f"The folder {data_path} exists and will not be created again")
---
+++ start.bat
@@ -0,0 +1,4 @@
+call "..\set_environment.bat"
+set cwd=%~dp0%
+echo on
+%python_interpreter% setup.py %cwd%
