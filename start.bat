91b181ba5e86e68684b95fcb253a33bf7e4cc869
Rahul Sharma <sharma.uce@gmail.com>
2025-07-19 18:42:42 +0200

    done

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 ::call "install_dependencies.bat"
 set cwd=%~dp0%
 echo on
-%python_interpreter% setup.py %cwd% 224 10 0.1 4 > start.log
+%python_interpreter% setup.py %cwd% 224 10 0.001 40 > start.log
